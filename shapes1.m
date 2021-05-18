d44b87909be00cd33cd5a115a0b7e6f610d47c2d
olghenitta <olghenitta@gmail.com>
2021-05-18 10:15:56 +0200

    8 & 4 stimulus; central circular mask
    Works for 8 and 4 stimulus
    Instead of inner arcs one central filled oval

--- shapes1.m
+++ shapes1.m
@@ -29,32 +29,35 @@ bgColor = [0.98 0.78 0.97];
 [xCenter, yCenter] = RectCenter(windowRect);
 
 % Svens's variables
-design.tarecc   = 4; 
-nstim           = 8;
-ang             = 0:2/nstim*pi:(2-1/nstim)*pi; 
 visual.ppd      = 51.556;
+
+nstim           = 8;
+design.tarecc   = 6; %dva
+tareccPix       = design.tarecc*visual.ppd;
+ang             = 0:2/nstim*pi:(2-1/nstim)*pi; 
 [dpx, dpy]      = pol2cart(ang,design.tarecc*visual.ppd);
 design.stiPosi  = round([dpx' dpy']);   % 1 is right relative center then clockwise     
 % Parameters for drawing stuff
 wurstRadDeg     = 1; % how far from ref. points my arcs are located (how thick is the wurst) 
 wurstRadPix     = visual.ppd*wurstRadDeg;  
-leftover        = 5.0;                 % degrees for arc ends relative ref points
+
 segment1 = 360/nstim;
+leftover        = 0.2* segment1;                 % degrees for arc ends relative ref points
 %rectangle relative stimulus Position
-rect72P = [design.stiPosi(nstim-(nstim/2-1),1)-wurstRadPix design.stiPosi(nstim-1,2)-wurstRadPix...
-    design.stiPosi(1,1)+wurstRadPix  design.stiPosi((nstim/2-1),2)+wurstRadPix];
-rect72out = [xCenter yCenter xCenter yCenter] + rect72P;
+% rect72P = [design.stiPosi(nstim-(nstim/2-1),1)-wurstRadPix design.stiPosi(nstim-1,2)-wurstRadPix...
+%     design.stiPosi(1,1)+wurstRadPix  design.stiPosi((nstim/2-1),2)+wurstRadPix];
+max(design.stiPosi(1,:))
+rect72out = [xCenter yCenter xCenter yCenter] - [tareccPix tareccPix -tareccPix  -tareccPix] - [wurstRadPix wurstRadPix -wurstRadPix -wurstRadPix];
 rectDecrease = [wurstRadPix wurstRadPix -wurstRadPix -wurstRadPix]*2;
 rect72in = rect72out +rectDecrease;
-%rectIncrease  = -rectDecrease
-rectImage = rect72out - rectDecrease*2;
+
 % Set the color of the rect to nice and princessy
 arcColor2 = [0.95 0.97 0.59];
 arcColor1 = [0.78 0.96 0.99];
 dotColor  = [0.91 0.70 0.97];
-draftColor = [0.5 0 0.5];
+draftColor = bgColor;%[0.5 0 0.5];
 % Take a random ref stimulus
-stim1 =  randi([1 nstim/2]);
+stim1 = 1;%randi([1 nstim/2]);
 switch stim1
     case 1
         rotateAll = 0;
@@ -69,13 +72,14 @@ end
 lineWidth = 3;
 arcAngle = segment1*(nstim/2-1);    
 Screen('FillArc',window,arcColor1,rect72out, 0-leftover+rotateAll, arcAngle+leftover*2)
-Screen('FillArc',window,draftColor,rect72in, 0-leftover+rotateAll, arcAngle+leftover*2)
+%Screen('FillArc',window,draftColor,rect72in, 0-leftover+rotateAll, arcAngle+leftover*2)
 
 Screen('FillArc',window,arcColor2,rect72out,180-leftover+rotateAll, arcAngle+leftover*2)
-Screen('FillArc',window,draftColor,rect72in,180-leftover+rotateAll, arcAngle+leftover*2)
+%Screen('FillArc',window,draftColor,rect72in,180-leftover+rotateAll, arcAngle+leftover*2)
 
 Screen('FrameRect', window, dotColor, rect72out, lineWidth);
 Screen('FrameRect', window, dotColor, rect72in, lineWidth);
+Screen('FillOval', window, draftColor, rect72in)
 
 Screen('DrawDots', window, [design.stiPosi(:,1)'; design.stiPosi(:,2)'], 15, dotColor, [xCenter, yCenter]);
 
